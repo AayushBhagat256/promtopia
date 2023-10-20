be6b0a70ac5ab87471ff7360e3457bd7f8c027d3
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-10-20 20:19:17 +0530

    basic homepage done

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,15 @@
 
 function Home() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI - powered prompts</span>
+      </h1>
+      <p className="desc text-center">An open source AI prompting tool Designed with objective to achive efficiency for AI tools on a user level. <br />Discover , create and share creative prompts</p>
+
+      {/* feed component  */}
+    </section>
   )
 }
 
