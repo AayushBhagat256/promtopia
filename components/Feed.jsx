889d84c2af773d9aedae91e16e03673c49292b5e
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-10-26 12:13:26 +0530

    feedAdded and copy field added

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+"use client"
+
+import { useState, useEffect } from "react"
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt, index) => (
+        <PromptCard
+          key={prompt.id}
+          post={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('')
+  const [post, setPost] = useState([]);
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value)
+    console.log(searchText)
+  }
+  useEffect(
+    () => {
+      const fetchPosts = async () => {
+        const response = await fetch('/api/prompt')
+        const data = await response.json();
+        setPost(data)
+      }
+      console.log(post)
+      fetchPosts()
+    }, []
+  )
   return (
-    <div>
-      Feed component
-    </div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input type="text"
+          placeholder="Search for prompts/tags/userName"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList
+        data={post}
+        handleTagClick={() => { }}
+
+      />
+    </section>
   )
 }
 
